a298edf969bc7cd296622ad958a6b7f709ea2fb4
Anton Ukhanev <xedin.unknown@gmail.com>
2019-03-12 13:54:24 +0100

    Added `DatabaseWpdbTrait`
    This abstracts database operations, allowing code to be
    de-coupled from WPDB.

---
+++ src/DatabaseWpdbTrait.php
@@ -0,0 +1,96 @@
+<?php
+
+namespace Inpsyde\MultilingualPress2to3;
+
+use Exception;
+use Throwable;
+use wpdb as Wpdb;
+
+/**
+ * Functionality for running database operations using WPDB.
+ *
+ * @package MultilingualPress2to3
+ */
+trait DatabaseWpdbTrait
+{
+
+    /**
+     * Inserts a record defined by provided data into a table with the specified name.
+     *
+     * @param string $name The name of the table to insert into.
+     * @param array|object $data The data of the record to insert.
+     *
+     * @return int|null The inserted row's ID, if applicable; otherwise null;
+     *
+     * @throws Throwable If problem inserting.
+     */
+    protected function _insert($name, $data) {
+        $data = (array) $data;
+        $db = $this->_getDb();
+
+        $db->insert($name, $data);
+        $error = $db->last_error;
+        $insertId = $db->insert_id;
+
+        // Error while inserting
+        if (empty($result) && !empty($error)) {
+            throw new Exception($error);
+        }
+
+        // No new insert ID generated
+        if ($insertId === 0) {
+            return null;
+        }
+
+        return $insertId;
+    }
+
+    /**
+     * Retrieves rows from the database matching the query conditions.
+     *
+     * @param string $query A query with optional `sprintf()` style placeholders.
+     * @param array $values A list of values for the query placeholders.
+     *
+     * @return object[] A list of records.
+     *
+     * @throws Throwable If problem selecting.
+     */
+    protected function _select($query, $values = []) {
+        $db = $this->_getDb();
+        $query = $db->prepare($query, $values);
+
+        $result = $db->get_results($query, 'OBJECT');
+        $error = $db->last_error;
+
+        // Error while selecting
+        if (empty($result) && !empty($error)) {
+            throw new Exception($error);
+        }
+
+        return $result;
+    }
+
+    /**
+     * Prefixes a table name.
+     *
+     * @param string $name The name to prefix.
+     *
+     * @return string The prefixed name.
+     *
+     * @throws Throwable If problem prefixing.
+     */
+    protected function _getPrefixedTableName($name) {
+        $prefix = $this->_getDb()->prefix;
+
+        return "{$prefix}$name";
+    }
+
+    /**
+     * Retrieves the WPDB adapter used by this instance.
+     *
+     * @return Wpdb
+     *
+     * @throws Throwable
+     */
+    abstract protected function _getDb();
+}
