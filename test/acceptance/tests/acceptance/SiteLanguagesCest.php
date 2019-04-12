a591850440c754c67a678a346f7b4671b8244608
dinamiko <e.castells@inpsyde.com>
2019-04-12 15:14:14 +0200

    Add acceptance test for site languages migration

---
+++ test/acceptance/tests/acceptance/SiteLanguagesCest.php
@@ -0,0 +1,53 @@
+<?php # -*- coding: utf-8 -*-
+declare(strict_types=1);
+
+class SiteLanguagesCest
+{
+    public function _before(AcceptanceTester $I)
+    {
+        $I->amOnPage('/wp-login.php');
+        $I->fillField(['name' => 'log'], 'admin');
+        $I->fillField(['name' => 'pwd'], 'password');
+        $I->click('#wp-submit');
+    }
+
+    public function migrateSiteLanguages(AcceptanceTester $I)
+    {
+        // MLP2 check site selected languages
+        $I->amOnPage('/wp-admin/network/site-settings.php?id=1&extra=mlp-site-settings');
+        $language = $I->grabValueFrom('#inpsyde_multilingual_lang');
+        $I->assertSame(trim($language), 'en_US');
+        $I->amOnPage('/wp-admin/network/site-settings.php?id=2&extra=mlp-site-settings');
+        $language = $I->grabValueFrom('#inpsyde_multilingual_lang');
+        $I->assertSame(trim($language), 'es_ES');
+        $I->amOnPage('/wp-admin/network/site-settings.php?id=3&extra=mlp-site-settings');
+        $language = $I->grabValueFrom('#inpsyde_multilingual_lang');
+        $I->assertSame(trim($language), 'it_IT');
+
+        // run the tool
+        $this->runTheTool($I);
+
+        // MLP3 check site selected languages
+        $I->amOnPage('/wp-admin/network/sites.php?page=multilingualpress-site-settings&id=1');
+        $language = $I->grabValueFrom('#mlp-site-language-tag');
+        $I->assertSame($language, 'en-US');
+        $I->amOnPage('/wp-admin/network/sites.php?page=multilingualpress-site-settings&id=2');
+        $language = $I->grabValueFrom('#mlp-site-language-tag');
+        $I->assertSame($language, 'es-ES');
+        $I->amOnPage('/wp-admin/network/sites.php?page=multilingualpress-site-settings&id=3');
+        $language = $I->grabValueFrom('#mlp-site-language-tag');
+        $I->assertSame($language, 'it-IT');
+    }
+
+    private function runTheTool(AcceptanceTester $I)
+    {
+        // deactivate MLP2 and activate MLP3
+        $I->amOnPage('/wp-admin/network/plugins.php');
+        $I->click('[data-plugin="multilingual-press/multilingual-press.php"] .deactivate a');
+        $I->click('[data-plugin="multilingualpress/multilingualpress.php"] .activate a');
+
+        // run the tool
+//        $I->runShellCommand('wp mlp2to3 unknown --path=wordpress-site');
+//        $I->seeInShellOutput('Success: Migration complete');
+    }
+}
