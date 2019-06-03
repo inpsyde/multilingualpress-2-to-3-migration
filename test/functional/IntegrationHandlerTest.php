532f2d6fb17d86a377eece3a96d7f69238699378
Anton Ukhanev <xedin.unknown@gmail.com>
2019-06-03 13:03:50 +0200

    Added test for `IntegrationHandler`

---
+++ test/functional/IntegrationHandlerTest.php
@@ -0,0 +1,72 @@
+<?php
+
+namespace Inpsyde\MultilingualPress2to3\Test\Functional;
+
+use Inpsyde\MultilingualPress\Framework\Cache\Exception\Exception;
+use Inpsyde\MultilingualPress2to3\IntegrationHandler as TestSubject;
+use Inpsyde\MultilingualPress2to3\Test\Helper\ComponentMockeryTrait;
+use PHPUnit\Framework\TestCase;
+use PHPUnit\Framework\MockObject\MockObject;
+use Andrew\Proxy;
+use function Brain\Monkey\setUp;
+use function Brain\Monkey\tearDown;
+use Throwable;
+
+class IntegrationHandlerTest extends TestCase
+{
+    use ComponentMockeryTrait;
+
+    protected function setUp()
+    {
+        parent::setUp();
+        setUp();
+    }
+
+    protected function tearDown()
+    {
+        parent::tearDown();
+        tearDown();
+    }
+
+    /**
+     * Creates a new instance of the test subject.
+     *
+     * @return MockObject|TestSubject The new instance.
+     * @throws Exception If problem creating.
+     * @throws Throwable If problem running.
+     */
+    protected function _createSubject($methods = [], $dependencies = [])
+    {
+        $subject = $this->createMockBuilder(TestSubject::class, $methods, $dependencies)
+            ->getMock();
+
+        return $subject;
+    }
+
+    public function testPreventSharedTableDeletion()
+    {
+        {
+            $names = $this->createArray(rand(3, 9), function ($i) {
+                return uniqid("table-$i-name");
+            });
+            $removedIndex = 1;
+            $filter = 'multilingualpress.deleted_tables';
+            $settings = $this->createContainer([
+                'shared_table_names' => [
+                    $names[$removedIndex],
+                ],
+                'filter_deleted_tables' => $filter,
+            ]);
+            $subject = $this->_createSubject(null, [$settings]);
+            $_subject = $this->proxy($subject);
+        }
+
+        {
+            $_subject->_preventLegacyCheck();
+            $expected = $names;
+            array_splice($expected, $removedIndex, 1);
+
+            $this->assertEquals($expected, apply_filters($filter, $names));
+        }
+    }
+}
