e6f3a5cc7594df6554acc52e215c63d5c4d4f34e
Anton Ukhanev <xedin.unknown@gmail.com>
2019-06-21 19:16:26 +0200

    Fixed test for `CompositeHandlerTrait`

---
+++ test/unit/Handler/CompositeHandlerTraitTest.php
@@ -0,0 +1,102 @@
+<?php
+
+namespace Inpsyde\MultilingualPress2to3\Test\Unit\Handler;
+
+use Inpsyde\MultilingualPress2to3\Handler\HandlerInterface;
+use Inpsyde\MultilingualPress2to3\Handler\CompositeHandlerTrait as TestSubject;
+use Inpsyde\MultilingualPress2to3\Test\Helper\ComponentMockeryTrait;
+use PHPUnit\Framework\MockObject\MockObject;
+use Andrew\Proxy;
+use PHPUnit\Framework\TestCase;
+
+/**
+ * Tests `RunHandlerListCapableTrait`.
+ *
+ * @package MultilingualPress2to3
+ */
+class CompositeHandlerTraitTest extends TestCase
+{
+    use ComponentMockeryTrait;
+
+    /**
+     * Creates a new instance of the test subject.
+     *
+     * @return MockObject|TestSubject The new instance.
+     */
+    protected function _createSubject($methods = [])
+    {
+        $subject = $this->getMockBuilder(TestSubject::class, $methods, null)
+            ->getMockForTrait();
+
+        return $subject;
+    }
+
+    /**
+     * Tests that the subject can be creates successfully.
+     */
+    public function testCreation()
+    {
+        $subject = $this->_createSubject();
+        $this->assertInternalType('object', $subject, 'A valid instance of the test subject could not be created');
+    }
+
+    /**
+     * Tests `_runHandlers()`.
+     *
+     * It must invoke the `run()` method on all handlers it is given.
+     */
+    public function testRun()
+    {
+        {
+            $subject = $this->_createSubject(null);
+            $handlerAmount = rand(2, 9);
+            $handlers = $this->_createHandlers($handlerAmount);
+            $proxy = $this->proxy($subject);
+            /* @var $proxy TestSubject */
+
+            foreach ($handlers as $handler) {
+                $handler->expects($this->exactly(1))
+                    ->method('run');
+            }
+
+            $subject->expects($this->exactly(1))
+                ->method('_getHandlers')
+                ->will($this->returnValue($handlers));
+        }
+
+        {
+            $proxy->_run();
+        }
+    }
+
+    /**
+     * Creates a mock of a handler.
+     *
+     * @return MockObject|HandlerInterface The new handler mock.
+     */
+    protected function _createHandler()
+    {
+        $mock = $this->getMockBuilder(HandlerInterface::class)
+            ->getMock();
+
+        return $mock;
+    }
+
+    /**
+     * Creates a list of handlers.
+     *
+     * @param int $amount The amount of handlers to create.
+     *
+     * @return HandlerInterface[]|MockObject[] The list of new handlers.
+     */
+    protected function _createHandlers(int $amount)
+    {
+        $handlers = [];
+
+        for ($i = 0; $i < $amount-1; $i++) {
+            $handlers[] = $this->_createHandler();
+        }
+
+        return $handlers;
+    }
+}
